dbdde3cf21adade397d3fb219aea43f10ba08dc3
momin210103 <momincse13@gmail.com>
2025-05-08 23:00:11 +0600

    Add files via upload
    multiplication of 2 digit number

---
+++ multiplication.asm
@@ -0,0 +1,43 @@
+.model small
+.stack 100h
+.data
+a db ?
+b db ?
+.code
+main proc 
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,1
+    int 21h
+    sub al,48
+    mov a,al
+    
+    mov ah,1
+    int 21h
+    sub al,48
+    mov b,al
+    
+    mul a
+    aam
+    mov bx,ax
+    
+    mov ah,2
+    mov dl,bh
+    add dl,48
+    int 21h
+    
+    mov ah,2
+    mov dl,bl
+    add dl,48
+    int 21h
+    
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
+    
+     
+    
